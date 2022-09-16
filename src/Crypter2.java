757dff86ad7335a0687185f51c6d03b118f1a70c
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-09-16 13:38:19 +0200

    Skapade en encrypter för strings men den funkar inte om outputten har tecken som inte syns i konsolen.

--- src/Crypter2.java
+++ src/Crypter2.java
@@ -2,14 +2,20 @@ import javax.swing.*;
 
 public class Crypter2 {
     public static void main(String[] args) {
-        int x = Integer.parseInt(JOptionPane.showInputDialog("Input a number"));
-        int y = Integer.parseInt(JOptionPane.showInputDialog("Input a key"));
-        char c = (char)Encrypt(x,y);
-        System.out.println(c);
+        String input = JOptionPane.showInputDialog("Write your string");
+        String key = JOptionPane.showInputDialog("Enter a key");
+        int x;
+        int y;
+        String output = "";
 
-    }
-    static int Encrypt(int x,int y){
-        int z =+ x^y;
-        return z;
+        for (int i = 0; i<input.length(); i++){
+            x =  input.charAt(i);
+            y =  key.charAt(i);
+            int z = x^y;
+            char c = (char)z;
+            output += c;
+        }
+        System.out.println(output);
+
     }
 }
