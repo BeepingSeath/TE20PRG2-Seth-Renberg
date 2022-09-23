6ba72078e4bb3cb74cbc9cfab5e5939e54237fa5
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-09-23 16:32:26 +0200

    GUI

---
+++ src/Crypter2GUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Crypter2GUI {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JScrollBar scrollBar1;
+    private JProgressBar progressBar1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Crypter2GUI");
+        frame.setContentPane(new Crypter2GUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        progressBar = new JProgressBar(0, task.getLengthOfTask());
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+    }
+}
